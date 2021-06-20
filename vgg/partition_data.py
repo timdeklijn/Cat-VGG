31bd92b20fcb6b48715aa0eb51fddf158b5d16c5
Tim de Klijn <timdeklijn@gmail.com>
2021-06-20 10:56:59 +0200

    Add automatic folder cleaning

--- vgg/partition_data.py
+++ vgg/partition_data.py
@@ -1,4 +1,5 @@
 from typing import List
+import os
 from shutil import copyfile
 from pathlib import Path
 
@@ -18,7 +19,24 @@ def copy_files(in_list: List[Path], out_path: Path):
         copyfile(f, Path(out_path, f.name))
 
 
-def partition_data(input_path: Path, output_path: Path, split=0.9):
+def clean_folders(folders: List[Path]):
+    """
+    Remove content all folders in input list.
+
+    Patameters
+    ----------
+    folders: List[Path]
+        list of folders
+    """
+    for f in folders:
+        files = f.glob("*")
+        if len(files) == 0:
+            continue
+        for ff in files:
+            os.remove(ff)
+
+
+def partition_data(input_path: Path, output_path: Path, split=0.7):
     """
     Split data and place in folders used to create generators from
 
@@ -42,12 +60,22 @@ def partition_data(input_path: Path, output_path: Path, split=0.9):
     rey_split = int(split * len(rey))
     rey_train, rey_eval = rey[:rey_split], rey[rey_split:]
 
-    # copy images to folders
-    copy_files(maz_train, Path(output_path, "train", "maz"))
-    copy_files(maz_eval, Path(output_path, "valid", "maz"))
+    # Paths to output folders
+    maz_train_folder = Path(output_path, "train", "maz")
+    maz_valid_folder = Path(output_path, "valid", "maz")
+    rey_train_folder = Path(output_path, "train", "rey")
+    rey_valid_folder = Path(output_path, "valid", "rey")
 
-    copy_files(rey_train, Path(output_path, "train", "rey"))
-    copy_files(rey_eval, Path(output_path, "valid", "rey"))
+    # Remove content of folders
+    clean_folders([maz_train_folder, maz_valid_folder,
+                   rey_train_folder, rey_valid_folder])
+
+    # copy images to folders
+    copy_files(maz_train, maz_train_folder)
+    copy_files(maz_eval, maz_valid_folder)
+
+    copy_files(rey_train, rey_train_folder)
+    copy_files(rey_eval, rey_valid_folder)
 
 
 if __name__ == "__main__":
